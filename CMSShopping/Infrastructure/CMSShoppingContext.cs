b4b845618f60623b2b06c8353a1cd082e0a59a4f
IhorZ <acere5ihor@gmail.com>
2021-10-06 18:50:37 +0300

    Add class context

---
+++ CMSShopping/Infrastructure/CMSShoppingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSShopping.Infrastructure
+{
+    public class CMSShoppingContext : DbContext
+    {
+        public CMSShoppingContext(DbContextOptions<CMSShoppingContext> options)
+            :base(options)
+        {
+        }
+
+
+    }
+}
